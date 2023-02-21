cf4e7fa1f502dfd607b45e3c099ba87c7f0d265a
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2023-02-21 15:13:34 -0800

    added php partials folder to build process

---
+++ public/partials/desktopNav.php
@@ -0,0 +1,51 @@
+<!-- Desktop Nav -->
+<nav class="nav nav--desktop">
+  <ul class="nav-list">
+    <li>
+      <a href="#" class="nav-list__item nav-list__item--current">Home</a>
+    </li>
+    <li><a href="#" class="nav-list__item">Pricing</a></li>
+    <li><a href="#" class="nav-list__item">Class Schedule</a></li>
+    <li><a href="#" class="nav-list__item">Curriculum</a></li>
+    <li><a href="#" class="nav-list__item">Updates & Events</a></li>
+    <li><a href="#" class="nav-list__item">Scholorship Program</a></li>
+    <li><a href="#" class="nav-list__item">About Us</a></li>
+    <li><a href="#" class="nav-list__item">The A-Team</a></li>
+    <li><a href="#" class="nav-list__item">FAQ</a></li>
+    <li><a href="#" class="nav-list__item">Code Of Conduct</a></li>
+    <li>
+      <a href="#" class="nav-list__item">
+        <!-- Instagram Icon -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="#000000"
+            d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"
+          ></path>
+        </svg>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="nav-list__item">
+        <!-- Facebook Icon -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="#000000"
+            d="M13.355 22v-9.123h3.062l.459-3.555h-3.52v-2.27c0-1.03.285-1.731 1.761-1.731L17 5.32V2.14A25.233 25.233 0 0 0 14.257 2c-2.715 0-4.573 1.657-4.573 4.7v2.622h-3.07v3.555h3.07V22h3.671Z"
+          ></path>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</nav>
---
+++ src/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/index.css" />
+    <title>A-Side Jiu Jitsu + Fitness</title>
+  </head>
+  <body>
+    <header class="header">
+      <div class="header__logo-container">
+        <a href="/">
+          <img
+            class="img"
+            src="./assets/Logo.jpg"
+            alt="A side Jiu Jitsu North Hollywood Logo"
+          />
+        </a>
+      </div>
+      <?php include "./partials/mobileNav.php" ?>
+      <?php include "./partials/desktopNav.php" ?>
+    </header>
+  </body>
+</html>
